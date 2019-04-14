6f4c23686296e40c98d850c4bdfff07619adee0f
jperalta <jperalta.henri@gmail.com>
2019-04-14 15:34:49 -0400

    contact section has buttons, now one page

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ function toAbout(){
 var about = document.querySelector("section");
 about.scrollIntoView({block: "start", behavior: "smooth"});
 }
+
+
+function toContact(){
+var contact = document.querySelector(".contact");
+contact.scrollIntoView({block: "start", behavior: "smooth"});
+}
